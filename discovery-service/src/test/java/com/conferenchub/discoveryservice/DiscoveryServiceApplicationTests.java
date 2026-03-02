93783ec27c28759b6b437cab48759ad1afc5ea28
Ayman Elh <elhamioui1964@gmail.com>
2026-03-02 14:22:02 +0000

    feat(discovery-service): set up initial discovery service with Eureka and Maven configuration

---
+++ discovery-service/src/test/java/com/conferenchub/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conferenchub.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
