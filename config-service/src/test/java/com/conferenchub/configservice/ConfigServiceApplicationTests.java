877e9df675d56376ad641b6a7de29e32c1da033b
Ayman Elh <elhamioui1964@gmail.com>
2026-04-09 17:32:09 +0100

    test: configure mandatory git properties and native profile for integration tests

--- config-service/src/test/java/com/conferenchub/configservice/ConfigServiceApplicationTests.java
+++ config-service/src/test/java/com/conferenchub/configservice/ConfigServiceApplicationTests.java
@@ -2,8 +2,14 @@ package com.conferenchub.configservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "CONFIG_GIT_URI=dummy",
+        "CONFIG_GIT_USER=dummy",
+        "CONFIG_GIT_PASSWORD=dummy"
+})
+@ActiveProfiles("native")
 class ConfigServiceApplicationTests {
 
     @Test
